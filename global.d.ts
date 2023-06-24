7b9bbc1f9601182f5d219c08bce6e6268e90326b
GeorgeSaeed <97341656+GeorgeSaeed@users.noreply.github.com>
2023-06-24 14:48:58 +0200

    added count to Array for checking it's visible in the referencing projects

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+interface Array<T>
+{
+    count(): number;
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+
+const arrProto = Array.prototype;
+arrProto.count = function()
+{
+    return this.length;
+}
+
 export function test()
 {
     console.log('8888 Modified from Gotcha and pushed');
