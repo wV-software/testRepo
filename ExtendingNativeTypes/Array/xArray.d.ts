7381e8d731d195d10147bf19815797d49b5196cc
GeorgeSaeed <97341656+GeorgeSaeed@users.noreply.github.com>
2023-06-24 16:42:41 +0200

    naming extension of ReadonlyArray problem

--- ExtendingNativeTypes/Array/xArray.d.ts
+++ ExtendingNativeTypes/Array/xArray.d.ts
@@ -1,16 +1,3 @@
-
-interface IxReadonlyArray<T>
-{
-    select<TReturn>(callbackfn: (value: T, index: number, array: T[]) => TReturn): TReturn[];
-    selectMany<TReturn>(func: (arg: T) => TReturn[]): TReturn[];
-    sort(comparer: (a: T, b: T) => number): T[];
-    reverse(): T[];
-    isAny(checker?: (arg: T) => boolean): boolean;
-    areAll(checker: (arg: T) => boolean): boolean;
-    isEmpty(): boolean;
-    tryGetFirst(checker: (i: T) => boolean): T | undefined;
-}
-
 interface IxArray<T> extends IxReadonlyArray<T>
 {
     addMany(items: T[]): void;
--- ExtendingNativeTypes/Array/xReadonlyArray.d.ts
+++ ExtendingNativeTypes/Array/xReadonlyArray.d.ts
@@ -1,4 +1,16 @@
+interface IxReadonlyArray<T>
+{
+    select<TReturn>(callbackfn: (value: T, index: number, array: T[]) => TReturn): TReturn[];
+    selectMany<TReturn>(func: (arg: T) => TReturn[]): TReturn[];
+    sort(comparer: (a: T, b: T) => number): T[];
+    reverse(): T[];
+    isAny(checker?: (arg: T) => boolean): boolean;
+    areAll(checker: (arg: T) => boolean): boolean;
+    isEmpty(): boolean;
+    tryGetFirst(checker: (i: T) => boolean): T | undefined;
+}
+
 declare interface ReadonlyArray<T>
 {
-    get x(): IxReadonlyArray<T>;
+    get y(): IxReadonlyArray<T>;
 }
